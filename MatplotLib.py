85c47bc6993a7fc71ffa1c90639e418648503ba5
MD Humayun Kabir <humayun.112358@gmail.com>
2024-05-14 01:37:55 +0600

    plot

--- MatplotLib.py
+++ MatplotLib.py
@@ -1,8 +1,16 @@
 import matplotlib.pyplot as plt
 
-x = [1,2,3,4,5]
-y = [1,2,3,4,5]
+x = [1,2,3,4,5,6,7,8]
+y = [2,4,6,8,10,12,14,16]
 
-graph = plt.plot(x,y)
+#graph = plt.plot(x,y)
+
+#graph = plt.scatter(x,y)
+
+#graph = plt.hist(x,bins=6, edgecolor='black')
+
+#graph = plt.bar(x,height=3)
+
+graph = plt.pcolor(C=[1,2,3])
 
 print(plt.show())
